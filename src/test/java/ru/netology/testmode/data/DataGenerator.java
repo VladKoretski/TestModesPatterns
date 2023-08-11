0eae58aab1b36702eae0a2c50dc78a54571e5612
Vladimir Koretskiy <130839671+VladKoretski@users.noreply.github.com>
2023-08-11 16:50:51 +0300

    Update DataGenerator.java

--- src/test/java/ru/netology/testmode/data/DataGenerator.java
+++ src/test/java/ru/netology/testmode/data/DataGenerator.java
@@ -14,7 +14,7 @@ import static io.restassured.RestAssured.given;
 public class DataGenerator {
     public static final RequestSpecification requestSpec = new RequestSpecBuilder()
             .setBaseUri("http://localhost")
-            .setPort(9999)
+            .setPort(7777)
             .setAccept(ContentType.JSON)
             .setContentType(ContentType.JSON)
             .log(LogDetail.ALL)
